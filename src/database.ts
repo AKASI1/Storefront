fee9364a682ccd91bb446a9e47fc0398161231e0
AKASI <ahmedkasi777@gmail.com>
2022-07-17 17:40:30 +0200

    fix issue

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,14 @@
-import "dotenv/config";
-import { Pool } from "pg";
+import 'dotenv/config';
+import { Pool } from 'pg';
 
 const db = new Pool({
   host: process.env.host,
   database:
-    process.env.NODE_ENV === "dev" ? process.env.database : process.env.test_db,
+    process.env.NODE_ENV === 'dev'
+      ? process.env.database
+      : process.env.test_db,
   user: process.env.user,
+  password: process.env.password,
 });
 
 export default db;
